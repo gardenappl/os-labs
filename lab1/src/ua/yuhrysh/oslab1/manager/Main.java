994478903ad70fe658a87b9909af6f32c20a822a
gardenapple <gardenapple@posteo.net>
2020-10-13 13:34:55 +0300

    Allow data input from user

--- lab1/src/ua/yuhrysh/oslab1/manager/Main.java
+++ lab1/src/ua/yuhrysh/oslab1/manager/Main.java
@@ -1,18 +1,55 @@
 package ua.yuhrysh.oslab1.manager;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.BinaryOperator;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        List<IntComputation> computations = Arrays.asList(
-                //new IntComputation(2, IntComputation.Type.F), //this returns 0
-                new IntComputation(3, IntComputation.Type.F), //this gets stuck
-                new IntComputation(0, IntComputation.Type.F),
-                new IntComputation(0, IntComputation.Type.G),
-                new IntComputation(1, IntComputation.Type.F),
-                new IntComputation(1, IntComputation.Type.G));
+        ArrayList<IntComputation> computations = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter function name (f or g), or 'start' to start computations: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("start")) {
+                if (computations.isEmpty()) {
+                    System.out.println("Please enter at least one term.");
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            IntComputation.Type computationType = null;
+            for (IntComputation.Type type : IntComputation.Type.values()) {
+                if (input.equalsIgnoreCase(type.getInternalName())) {
+                    //System.out.println("equal to " + type.getInternalName());
+                    computationType = type;
+                }
+            }
+            if (computationType == null) {
+                System.out.println("Invalid function name.");
+                continue;
+            }
+
+            System.out.print("Enter function argument: ");
+            int arg = -1;
+            try {
+                input = scanner.nextLine();
+                arg = Integer.parseInt(input);
+            } catch (NumberFormatException ignored) {
+            }
+            if (arg < 0 || arg > 5) {
+                System.out.println("Invalid argument. Please enter integer in the [0; 5] range.");
+                continue;
+            }
+            computations.add(new IntComputation(arg, computationType));
+            if (computations.size() > 6) {
+                System.out.println("Warning! Setup script only creates 6 FIFOs.");
+                System.out.println("If you run this without any additional setup the program will not work.");
+            }
+        }
 
         BinaryOperator<Integer> operator = (Integer a, Integer b) -> a * b;
 
